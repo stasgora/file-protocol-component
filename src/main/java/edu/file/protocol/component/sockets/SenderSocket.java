0cab2b69ec468cbca11092a4e7c35d4a82b4b103
Stanisław Góra <stasgora@gmail.com>
2019-04-27 18:12:14 +0200

    file send progress reporting

--- src/main/java/edu/file/protocol/component/sockets/SenderSocket.java
+++ src/main/java/edu/file/protocol/component/sockets/SenderSocket.java
@@ -11,6 +11,8 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -35,13 +37,24 @@ public class SenderSocket extends TransferSocket {
 			byte[] fileBytes = Files.readAllBytes(file.toPath());
 			sendParameters(fileBytes.length, clientRSAKey);
 			output.writeUTF(cryptoComponent.RSAEncrypt(cryptoComponent.getSessionKey(), clientRSAKey));
-			output.write(cryptoComponent.AESEncrypt(fileBytes, cryptoComponent.getSessionKey()));
+			sendFile(fileBytes);
 		} catch (SocketTimeoutException e) {
 			LOGGER.log(Level.WARNING, "Socket timeout", e);
 			eventHandler.reportStatus(ConnectionStatus.TIMEOUT);
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, "Socket error", e);
 			eventHandler.reportStatus(ConnectionStatus.ERROR);
+		}
+	}
+
+	private void sendFile(byte[] file) throws IOException {
+		byte[] encryptedFile = cryptoComponent.AESEncrypt(file, cryptoComponent.getSessionKey());
+		int bytesSent = 0;
+		while (bytesSent < encryptedFile.length) {
+			int nextByteIndex = bytesSent + BUFFER_SIZE <= encryptedFile.length ? bytesSent + BUFFER_SIZE : encryptedFile.length;
+			output.write(Arrays.copyOfRange(encryptedFile, bytesSent, nextByteIndex));
+			bytesSent = nextByteIndex;
+			eventHandler.reportTransferProgress(bytesSent / (double) encryptedFile.length);
 		}
 	}
 
